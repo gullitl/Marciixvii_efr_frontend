43dfa7685cef3ae760d261f96e168b7039f956a7
plusembo <plam.l@live.fr>
2020-05-25 15:13:02 -0400

    Remove user register form

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -4,9 +4,8 @@ import { RoutesRoutingModule } from './routes-routing.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './sessions/login/login.component';
-import { RegisterComponent } from './sessions/register/register.component';
 
-const COMPONENTS = [DashboardComponent, LoginComponent, RegisterComponent];
+const COMPONENTS = [DashboardComponent, LoginComponent];
 const COMPONENTS_DYNAMIC = [];
 
 @NgModule({
